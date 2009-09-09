37eb15a0fc0c7bed453168b5ad829aab29344999
radman <radu.a.vlad@gmail.com>
2009-09-08 18:07:02 -0700

    refactoring - removed obsolete generators

--- generators/request/request_generator.rb
+++ generators/request/request_generator.rb
@@ -3,9 +3,6 @@ class RequestGenerator < Rails::Generator::NamedBase
     record do |r|
       r.directory 'app/models/requests'
       r.template 'request_subclass.rb.erb', "app/models/requests/#{singular_name}_request.rb"
-      
-      r.directory 'app/views/request_mailer'
-      r.template 'mailer_view.erb', "app/views/request_mailer/#{singular_name}.erb"
     end
   end
 end
--- generators/request_mailer/request_mailer_generator.rb
+++
@@ -1,9 +0,0 @@
-class RequestMailerGenerator < Rails::Generator::Base
-  
-  def manifest
-    record do |r|
-      r.template 'mailer.rb.erb', 'app/models/request_mailer.rb'
-    end
-  end
-  
-end
