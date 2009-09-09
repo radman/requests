defae5797e540d699178dc81c7267705cc386007
radman <radu.a.vlad@gmail.com>
2009-09-08 18:04:15 -0700

    refactoring - extracted a validation from one of the observer methods

--- lib/request.rb
+++ lib/request.rb
@@ -1,9 +1,25 @@
 class Request < ActiveRecord::Base
   validates_presence_of :recipient_email
-
+  validate_on_create :is_unique_request
+  
   def accept!
     self.response = :accept
     self.save!
   end
 
+  def created
+    raise "Request.created must be implemented by all subclasses of Request"
+  end
+  
+  def accepted
+    raise "Request.accepted must be implemented by all subclasses of Request"
+  end
+  
+  protected
+  
+  def is_unique_request
+    if Request.exists?(:recipient_email => recipient_email, :type => type, :response => :none)
+      errors.add_to_base("There is already an outstanding request.")
+    end  
+  end
 end
--- lib/request_observer.rb
+++ lib/request_observer.rb
@@ -1,11 +1,5 @@
-class RequestObserver < ActiveRecord::Observer
-  def before_create(request)
-    # TODO: somehow make into a validation
-    if Request.exists?(:recipient_email => request.recipient_email, :type => request.type, :response => :none)
-      request.errors.add_to_base("There is already an outstanding request.")
-      return false
-    end
-    
+class RequestObserver < ActiveRecord::Observer  
+  def before_create(request)  
     request.token = generate_token
   end
 
@@ -14,15 +8,11 @@ class RequestObserver < ActiveRecord::Observer
   end
   
   def before_update(request)
-    if request.changed.include?('response')
-      request.responded_at = DateTime.now
-    end
+    request.responded_at = DateTime.now if request.changed.include?('response')
   end
   
   def after_update(request)
-    if request.changed.include?('response')
-      request.accepted if request.response == :accept
-    end
+    request.accepted if request.changed.include?('response') && request.response == :accept
   end
 
   private
@@ -30,5 +20,4 @@ class RequestObserver < ActiveRecord::Observer
   def generate_token
     Digest::SHA1.hexdigest([Time.now, rand].join)
   end
-  
 end
