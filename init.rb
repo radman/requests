d476f575776582a59ec442a470c89ba136d4236b
radman <radu.a.vlad@gmail.com>
2009-09-22 15:08:24 -0700

    plugin no longer using RequestObserver

--- init.rb
+++ init.rb
@@ -1,7 +1,6 @@
 require 'digest'
 
-requests_path=File.join(RAILS_ROOT,'app','models','requests')
+requests_path = File.join(RAILS_ROOT, 'app', 'models', 'requests')
 $: << requests_path
 
 ActiveSupport::Dependencies.load_paths << requests_path
-ActiveRecord::Base.observers << :request_observer
